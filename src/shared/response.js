647bb283fa72070ca9309a475c8bcde8d2e1beb2
aloerina01 <kiss_mint27@yahoo.co.jp>
2018-11-25 17:38:54 +0900

    Modify response saample

--- src/shared/response.js
+++ src/shared/response.js
@@ -1,5 +1,40 @@
 const response = {
-  items: [1, 3, 4],
+  items: [{
+    id: 100,
+    title: '1. Resist Google’s Attempts to Turn You Into a Robot',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: '' 
+  }, {
+    id: 101,
+    title: '2. What Happened When I Peeked Into My Node_Modules Directory',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }, {
+    id: 102,
+    title: '3. Why React Hooks, and how did we even get here?',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }, {
+    id: 103,
+    title: '4. These are the concepts you should know in React.js (after you learn the basics)',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }, {
+    id: 104,
+    title: '5. Monorepos Made Easier with Bit and NPM',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }, {
+    id: 105,
+    title: '6. Concepts to become an advanced React developer',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }, {
+    id: 106,
+    title: '7. Understanding Asynchronous JavaScript — the Event Loop',
+    src: 'https://via.placeholder.com/400x225.jpg',
+    description: ''
+  }],
   userSegment: {
     sex: 'M',
     age: '20',
