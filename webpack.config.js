dd36af2b2e41046941867362b98162f14a5eaa9c
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2019-01-11 19:56:24 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const __MODE__ = process.env.NODE_ENV; 
+const __DEV__ = __MODE__ === 'development';
+const __TARGET__ = process.env.TARGET || 'mobx'
+
+module.exports = {
+  mode: __MODE__ || 'development',
+  entry: `./src/${__TARGET__}/main.tsx`,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$|`\.tsx$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      shared: './src/shared'
+    }
+  }
+}
